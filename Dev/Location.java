3514dc06eadbcb554c9b970ed712336555983a1b
RandomlyKnighted <thughes@wktechnologies.com>
2012-11-21 21:30:59 -0600

    Started Location.java

--- Dev/Location.java
+++ Dev/Location.java
@@ -1,19 +1,106 @@
-public class Location {
-  /**Variables**/
+public class Location
+{
+	/*
+	*	The board is basically a 2D array of locations in which
+	*	either a flag, bomb, or number will be stored.
+	*
+	*	This class contains methods which get and set whether
+	*	each location contains either a flag, bomb, or number.
+	*	It also has a method which allows you to get and set
+	*	the distance from the nearest mine.
+	*/
+
+
+	//	Declare all variables
+	boolean flag;
+	boolean bomb;
+	boolean numberBool;
+	int number;
   
-  
-  /**Constructor**/
-  public Location() {
-    //Commented out because when the board calls it a million
-    //times, there's way too much shit being printed to the
-    //terminal
-    //System.out.println("Temporary constructor to build the board.");
-    //System.out.println("If this printed, that means it's working!");
-  }
-  
-  /**Functions**/
-  
-  
-  /**Driver**/
-  
+	//	Constructor
+	public Location()
+	{
+		//Commented out because when the board calls it a million
+		//times, there's way too much shit being printed to the
+		//terminal
+		//System.out.println("Temporary constructor to build the board.");
+		//System.out.println("If this printed, that means it's working!");
+		flag = false;
+		bomb = false;
+		numberBool = false;
+		number = 0;
+		
+	}
+	  
+	//	Getters
+	public Boolean hasFlag()
+	{
+		if (flag)
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+	
+	public Boolean hasBomb()
+	{
+		if (bomb)
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+	
+	public Boolean hasNumber()
+	{
+		if (numberBool)
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+	
+	// Setters
+	
+	// Returns a boolean so that you know whether not the flag was
+	// successfully set
+	public Boolean setFlag(boolean flag)
+	{
+		this.flag = flag;
+		return true;
+	}
+	
+	// Returns a boolean so that you know whether not the bomb was
+	// successfully set
+	public Boolean setBomb(boolean bomb)
+	{
+		this.bomb = bomb;
+		return true;
+	}
+	
+	// Returns a boolean so that you know whether not the number in the
+	// location was set successfully
+	public Boolean setNumber(int number)
+	{
+		this.number = number;
+		return true;
+	}
+	
+	//	Driver
+	public static void main(String[] args)
+	{
+	
+	}
 }

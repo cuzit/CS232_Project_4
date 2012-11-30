69127b4fc614d4bbe37a765cc709f91b04a4f745
RandomlyKnighted <thughes@wktechnologies.com>
2012-11-30 13:19:02 -0600

    Updated BoardUI

--- Dev/Cascade.java
+++ Dev/Cascade.java
@@ -5,45 +5,52 @@ public class Cascade {
     this.board = board;
   }
   
-public void cascade(int x, int y)
-{
-    //This code structure comes almost directly from the
-    //determineNumberofNearbyMines function from the
-    //Board class. If you need to see the logic of how
-    //this works, look for the comments there.
-    for(int i = (x - 1); i < (x + 2); i++) {
-      if(i >= 0 && i < board.returnWidth()) {
-	for(int j = (y - 1); j < (y + 2); j++) {
-	  if(j >= 0 && j < board.returnHeight()) {
-	    if(board.returnNumber(i, j) == 0) {
-	      //Right here should be the code to "pop"
-	      //the button and make it dissappear, replacing
-	      //it with an image for the number or whatever
-	      
-	      //Changing the number stored in a Location
-	      //should ONLY be done in the Cascade function
-	      //and NOWHERE else, as it could break things.
-	      //It is safe to do here because it keeps the
-	      //cascade function from calling itself
-	      //endlessly because of the recursion. Besides,
-	      //after a Location is cleared from the Board,
-	      //the number it stores is permanently irrelevant.
-	      board.setNumber(i, j, -1);
-	      
-	      cascade(i, j);
-	    }
-	    
-	    if(board.returnNumber(i, j) != -1) {
-	      if(board.returnBomb(i, j) == false) {
-		//Pop it
-		board.setNumber(i, j, -2);
-	      }
-	    }
-	  }
+	public void cascade(int x, int y)
+	{
+		//This code structure comes almost directly from the
+		//determineNumberofNearbyMines function from the
+		//Board class. If you need to see the logic of how
+		//this works, look for the comments there.
+		for(int i = (x - 1); i < (x + 2); i++)
+		{
+			if(i >= 0 && i < board.returnWidth())
+			{
+				for(int j = (y - 1); j < (y + 2); j++)
+				{
+					if(j >= 0 && j < board.returnHeight())
+					{
+						if(board.returnNumber(i, j) == 0)
+						{
+						  //Right here should be the code to "pop"
+						  //the button and make it dissappear, replacing
+						  //it with an image for the number or whatever
+						  
+						  //Changing the number stored in a Location
+						  //should ONLY be done in the Cascade function
+						  //and NOWHERE else, as it could break things.
+						  //It is safe to do here because it keeps the
+						  //cascade function from calling itself
+						  //endlessly because of the recursion. Besides,
+						  //after a Location is cleared from the Board,
+						  //the number it stores is permanently irrelevant.
+						  board.setNumber(i, j, -1);
+						  
+						  cascade(i, j);
+						}
+					
+						if(board.returnNumber(i, j) != -1)
+						{
+							if(board.returnBomb(i, j) == false)
+							{
+								//Pop it
+								board.setNumber(i, j, -2);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-      }
-    }
-  }
   
   public static void main(String[] args) {
     System.out.println("Running...");
